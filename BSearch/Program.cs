f258652587004630413baf6d5b143d21954b0c32
Philip Gelinas <philippeantoineg@canadel.ca>
2021-08-11 11:44:11 -0400

    Find all possible array values, Calculate iterations

--- BSearch/Program.cs
+++ BSearch/Program.cs
@@ -6,28 +6,38 @@ namespace BSearch
     {
         static void Main(string[] args)
         {
-            // found after x move
-            //foreach item in array try position
-            // use test asset 
-            //
+            // generate a long list of number
+            // sort them if unsorted
+            // get average of iteration based on amount of values in the array
+            // use random generator to get arrays of different size
 
             int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
-            int pointer = arr.Length / 2;
-            int min = 0;
-            int max = arr.Length;
+            int pointer;
+            int min;
+            int max;
+            int iteration;
+            bool positionFound;
+
 
             foreach (var item in arr)
             {
+                pointer = arr.Length / 2;
+                min = 0;
+                max = arr.Length;
+                iteration = 0;
+                positionFound = false;
                 Console.WriteLine($"Trying to find {item}");
 
                 while (arr[pointer] != item)
                 {
+                    iteration++;
                     if (arr[pointer] > item)
                     {
                         max = pointer - 1;
                         if (max == min)
                         {
-                            Console.WriteLine($"item {item} found at position {max}");
+                            Output(item, max, iteration);
+                            positionFound = true;
                             break;
                         }
                         else
@@ -36,7 +46,8 @@ namespace BSearch
                         }
                         if (arr[pointer] == item)
                         {
-                            Console.WriteLine($"item {item} found at position {pointer}");
+                            Output(item, pointer, iteration);
+                            positionFound = true;
                             break;
                         }
                     }
@@ -44,9 +55,10 @@ namespace BSearch
                     {
                         min = pointer + 1;
 
-                        if (max == min)
+                        if (min == max)
                         {
-                            Console.WriteLine($"item {item} found at position {max}");
+                            Output(item, min, iteration);
+                            positionFound = true;
                             break;
                         }
                         else
@@ -55,15 +67,24 @@ namespace BSearch
                         }
                         if (arr[pointer] == item)
                         {
-                            Console.WriteLine($"item {item} found at position {pointer}");
+                            Output(item, pointer, iteration);
+                            positionFound = true;
                             break;
                         }
                     }
                 }
-                Console.WriteLine($"item {item} found at position {pointer}");
-                break;
-
+                if (!positionFound)
+                {
+                    Output(item, pointer, iteration);
+                    continue;
+                }
             }
+        }
+        static void Output(int number, int position, int iteration)
+        {
+            Console.WriteLine($"Number: {number} Position: {position}");
+            Console.WriteLine($"Iteration: {iteration}");
+            Console.WriteLine();
         }
     }
 }
